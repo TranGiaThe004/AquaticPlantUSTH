f5aad6732e6b983da87d4d4d0075f7bb1cad269b
Trần Gia Thế <giathe0901@gmail.com>
2025-12-11 14:19:07 +0700

    sua vai loi nho

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,6 +12,10 @@ return new class extends Migration
             $table->id(); // BIGINT UNSIGNED AUTO_INCREMENT
             $table->string('name');
             $table->string('email')->unique();
+
+            // 👉 Thêm dòng này để fix lỗi seed
+            $table->timestamp('email_verified_at')->nullable();
+
             $table->string('password');
 
             // Thêm đúng như file SQL
@@ -19,6 +23,9 @@ return new class extends Migration
                   ->default('user');
             $table->string('avatar')->nullable();
             $table->text('bio')->nullable();
+
+            // 👉 Thêm dòng này để hỗ trợ "remember me"
+            $table->rememberToken();
 
             $table->timestamps(); // created_at, updated_at
         });
